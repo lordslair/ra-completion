2b2191a087784344b2eb2fdc91f5b74fd212ef1a
lordslair <lordslair@toolbox>
2016-11-21 14:26:50 +0000

    Added else statement

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,8 @@
 
 if [ $1 == 'webserver' ]
 then
-HOME='/home/ra_bot'
-su - ra_bot -c "/usr/bin/php -d session.save_path=$HOME/tmp -S 0.0.0.0:8000 $HOME/phpliteadmin.php > $HOME/tmp/nohup.log"
+    HOME='/home/ra_bot'
+    su - ra_bot -c "/usr/bin/php -d session.save_path=$HOME/tmp -S 0.0.0.0:8000 $HOME/phpliteadmin.php > $HOME/tmp/nohup.log"
+else
+    bash
 fi
