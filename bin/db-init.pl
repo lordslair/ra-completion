dcd799fc6cf4d76633a66662cc215cdcac473784
lordslair <lordslair@toolbox>
2016-11-21 14:29:21 +0000

    Only creates DB if non exists

--- bin/db-init.pl
+++ bin/db-init.pl
@@ -4,11 +4,18 @@ use warnings;
 
 use DBI;
 
-my $dbh = DBI->connect(
-    "dbi:SQLite:dbname=/home/ra_bot/ra_completion.db",
-    "",
-    "",
-    { RaiseError => 1 },
-) or die $DBI::errstr;
+if ( -f "/home/ra_bot/ra_completion.db" )
+{
+    my $dbh = DBI->connect(
+        "dbi:SQLite:dbname=/home/ra_bot/ra_completion.db",
+        "",
+        "",
+        { RaiseError => 1 },
+    ) or die $DBI::errstr;
 
-$dbh->do("CREATE TABLE Users(Id INT PRIMARY KEY, user_twitter TEXT, user_ra TEXT, ack TEXT, help TEXT, done_normal TEXT, done_hardcore TEXT)")
+    $dbh->do("CREATE TABLE Users(Id INT PRIMARY KEY, user_twitter TEXT, user_ra TEXT, ack TEXT, help TEXT, done_normal TEXT, done_hardcore TEXT)")
+}
+else
+{
+    print "DB already exists, doin' nothin'\n";
+}
