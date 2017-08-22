34f0e52c57a060c31966d96e7782432bce902501
lordslair <rvandepo+github@gmail.com>
2017-08-22 09:24:03 -0400

    Added Mentions subs

--- docker/lib/RAB/Twitter.pm
+++ docker/lib/RAB/Twitter.pm
@@ -17,6 +17,55 @@ my $twitter = Net::Twitter::Lite::WithAPIv1_1->new(
     ssl => 1,
 );
 
+sub getMentions
+{
+    my %Mentions;
+    my $lastmsg_id  = $twitter->mentions({ count => 1 });
+    while ( )
+    {
+        my $mentions    = $twitter->mentions({ max_id => $lastmsg_id, count => 20 });
+        for my $mention ( @$mentions )
+        {
+            $Mentions{$mention->{id}}{'sender_id'}  = $mention->{user}{id};
+            $Mentions{$mention->{id}}{'created_at'} = $mention->{created_at};
+            $Mentions{$mention->{id}}{'text'}       = $mention->{text};
+            $Mentions{$mention->{id}}{'sender'}     = $mention->{user}{name};
+
+            $lastmsg_id = $mention->{id} - 1;
+        }
+        if ( scalar(@$mentions) != 20 ) { last }
+    }
+
+    $lastmsg_id  = $twitter->user_timeline({ count => 1 });
+    while ( )
+    {
+        my $replies    = $twitter->user_timeline({ max_id => $lastmsg_id, count => 20 });
+        for my $reply ( @$replies )
+        {
+            my $id = $reply->{in_reply_to_status_id};
+            if ( $id )
+            {
+            # This tweet is already a response to someone's mention
+                $Mentions{$id}{'replied'} = 'yes';
+            }
+            else { }
+            $lastmsg_id = $reply->{id} - 1;
+        }
+        if ( scalar(@$replies) != 20 ) { last }
+    }
+    return \%Mentions;
+}
+
+sub SenderName
+{
+    my $id = shift;
+
+    my $user     = $twitter->show_user({ user_id => $id });
+    my $username = $user->{'screen_name'};
+
+    return $username;
+}
+
 sub Statuses
 {
     my $lastmsg_id  = $twitter->direct_messages({ count => 1 });
