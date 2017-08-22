155d5538190bd8615daa1ec244294a19f1f7937f
lordslair <rvandepo+github@gmail.com>
2017-08-22 08:48:29 -0400

    Fixed wrong path

--- docker/lib/RAB/Twitter.pm
+++ docker/lib/RAB/Twitter.pm
@@ -6,7 +6,7 @@ use YAML::Tiny;
 #
 # Variables initialization
 #
-my $twitterfile = './twitter-config.yaml';
+my $twitterfile = '/home/ra_bot/twitter-config.yaml';
 my $twittyaml = YAML::Tiny->read( $twitterfile );
 my $twitter = Net::Twitter::Lite::WithAPIv1_1->new(
     access_token_secret => $twittyaml->[0]{AccessTokenSecret},
