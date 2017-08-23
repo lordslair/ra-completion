383418d1dd79bdc960b43d60d59276318aa88bb5
lordslair <lordslair@users.noreply.github.com>
2017-08-23 16:08:36 +0200

    Deleted the now useless start.sh

--- docker/bin/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $1 == 'webserver' ]
-then
-    HOME='/home/ra_bot'
-    su - ra_bot -c "/usr/bin/php -d session.save_path=$HOME/tmp -S 0.0.0.0:8000 $HOME/phpliteadmin.php > $HOME/tmp/nohup.log"
-else
-    bash
-fi
