cc9472900e6e33811677c4a49c302d607e564d1f
lordslair <rvandepo+github@gmail.com>
2020-01-22 15:52:36 +0000

    First commit to start with k8s

---
+++ code/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "`date +"%F %X"` Building Perl dependencies and system set-up ..."
+
+apk update \
+    && apk add --no-cache perl perl-libwww perl-dbi perl-dbd-sqlite imagemagick6-dev perl-net-ssleay \
+    && apk add --no-cache --virtual .build-deps \
+                                    curl make perl-dev libc-dev gcc bash tzdata \
+    && curl -L https://cpanmin.us | perl - App::cpanminus --no-wget \
+    && cpanm --no-wget YAML::Tiny \
+                       File::Pid \
+                       Net::Twitter::Lite \
+                       Net::OAuth \
+    && cpanm --no-wget Image::Magick --force \
+    && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
+    && apk del .build-deps \
+    && rm -rf /root/.cpanm
+
+echo "`date +"%F %X"` Build done ..."
+
+exec /code/ra-completion
