ae1165574ef86518036f02d85120c14a6d3ac9d7
lordslair <5469390+lordslair@users.noreply.github.com>
2021-10-19 19:06:15 +0200

    Pinned to ImageMagick:6.9 build

--- code/entrypoint.sh
+++ code/entrypoint.sh
@@ -3,14 +3,15 @@
 echo "`date +"%F %X"` Building Perl dependencies and system set-up ..."
 
 apk update \
-    && apk add --no-cache perl perl-libwww perl-dbi perl-dbd-sqlite imagemagick6-dev perl-net-ssleay \
+    && apk add --no-cache perl perl-libwww perl-dbi perl-dbd-sqlite perl-net-ssleay \
+                          imagemagick6-dev \
     && apk add --no-cache --virtual .build-deps \
                                     curl make perl-dev libc-dev gcc bash tzdata \
     && curl -L https://cpanmin.us | perl - App::cpanminus --no-wget \
     && cpanm --no-wget File::Pid \
                        Net::Twitter::Lite \
                        Net::OAuth \
-    && cpanm --no-wget Image::Magick --force \
+    && cpanm --no-wget JCRISTY/Image-Magick-6.9.12-1.tar.gz \
     && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
     && apk del .build-deps \
     && rm -rf /root/.cpanm
